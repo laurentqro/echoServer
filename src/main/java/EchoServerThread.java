c2fd87ba60bfe615db0d67a7137e5650d0eb1426
Laurent Curau <laurentcurau@gmail.com>
2018-05-29 11:49:20 +0100

    Stop writing to the socket twice

--- src/main/java/EchoServerThread.java
+++ src/main/java/EchoServerThread.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 public class EchoServerThread extends Thread {
@@ -18,11 +17,9 @@ public class EchoServerThread extends Thread {
             System.out.println("Accepted client on thread: " + Thread.currentThread().getName());
             server.addClient(socket);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
             String message;
             while ((message = in.readLine()) != null) {
-                out.println(message);
                 server.broadcastMessage(message);
             }
             socket.close();
